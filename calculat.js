b63b4efdee41006ed9c95c49698bf36c77657cf7
eugene <eugenemugiraneza2@gmail.com>
2023-04-18 19:09:08 +0200

    detail

--- calculat.js
+++ calculat.js
@@ -1,11 +1,22 @@
-import React from 'react'
 
-const calculat = () => {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  )
-}
 
-export default calculat
+let names=['eugene','mugiraneza','27']
+console.log(names.length)
+const all=[{
+    id:1,
+    name:'eugene',
+    price:'123'
+
+
+},
+{
+    id:1,
+    name:'peter',
+    price:'67'
+
+
+}]
+
+all.map((e)=>{
+  console.log(e.name,e.price)  
+})
